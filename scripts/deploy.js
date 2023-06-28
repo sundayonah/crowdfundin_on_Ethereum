b26b068b9efa3ab7085641e102f8210b12ce4922
sundayonah <sundayonah94@gmail.com>
2023-06-28 10:06:36 +0100

    contract deployed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,15 +3,17 @@ const hre = require("hardhat");
 
 async function main() {
 
-  const CroudFunding = await hre.ethers.getContractFactory("Lock");
-  const croudFunding = await CroudFunding.deploy();
+  const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
+  const crowdFunding = await CrowdFunding.deploy();
 
-  await lock.deployed();
+  await crowdFunding.deployed();
 
-  console.log( `deployed to ${lock.address}`);
+  console.log( `CrowdFunding deployed to ${crowdFunding.address}`);
+}
 
+//CONTRACT ADDRESS   0x5FbDB2315678afecb367f032d93F642f64180aa3
+  
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-}
