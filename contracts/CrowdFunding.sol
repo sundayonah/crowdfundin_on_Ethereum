7e28c319223e5dd5cb3ef96ed65c8a9ea54281af
sundayonah <sundayonah94@gmail.com>
2023-06-28 09:44:34 +0100

    done with smart contract

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -54,6 +54,19 @@ function donateToCampaign(uint256 _id) public payable{
     }
 }
 
+function getDonators(uint256 _id) public view returns(address[] memory, uint256[] memory) {
+return (campaigns[_id].donators, campaigns[_id].donations);
+}
+
+function getCampaigns() public view returns(Campaign[] memory){
+    Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+    for( uint i =0; i < numberOfCampaigns; i++){
+        Campaign storage item = campaigns[i];
+
+          allCampaigns[i] = item;
+        }
+        return allCampaigns;
+}
 
 }
 
